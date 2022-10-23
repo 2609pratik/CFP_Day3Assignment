aa6a6103064af1898ac150c3fb76e44cccd5a386
2609pratik <pratikbhagadkar2609@gmail.com>
2022-10-23 18:21:17 +0530

    second commit

--- src/main/java/com/bridgelabz/greeting/controller/greetingController.java
+++ src/main/java/com/bridgelabz/greeting/controller/greetingController.java
@@ -23,31 +23,33 @@ public class greetingController {
 //		obj.put("name","sonoo");
 //		return service.getMessage();
 //	}
-	
+	//UC2
 	@GetMapping("/")
 	public String message() {
 		return service.getMessage();
 	}
+	//UC3
 	@GetMapping("/{firstName}/{lastName}")
 	public String greetMessage(@PathVariable String firstName , @PathVariable String lastName ) {
 		return service.getGreetMessage(firstName,lastName);
 	}
+	//UC3
 	@GetMapping("/{name}")
 	public String greetMessagesF(@PathVariable String name ) {
 		return service.getGreetMessageF(name);
 	}
-	//add message in database
+	////UC3 add message in database
 	@PostMapping("/add")
 	public greetingModel addMsg(@RequestBody greetingModel data) {
 		return service.addGreetMsg(data);
 	}
-	//Retrieve 
+	////UC5 Retrieve 
 	@GetMapping("/message/{id}")
 	public greetingModel getmessage(@PathVariable int id) {
 		greetingModel response = service.getGreetMsg(id);
 		return response;
 	}
-	//get all greeting message
+	////UC6 get all greeting message
 	@PostMapping("/getAll")
 	public List<greetingModel> getmessage() {
 		return service.getMsg();
